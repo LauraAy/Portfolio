5b6cf0985db56f9929963f9372cda3ab87b72194
LauraAy <laura.ay1564@gmail.com>
2020-03-30 17:43:22 -0700

    working yet again

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import {
 const Navbar = () => {
   return (
     <nav class="navbar navbar-default navbar-fixed-top navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" >Laura Aydelotte</a>
+      <Link to="/" className="navbar-brand" >Laura Aydelotte</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
--- src/components/PortfolioCards/PortfolioCards.js
+++ src/components/PortfolioCards/PortfolioCards.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import {
+  Link
+} from 'react-router-dom'
 
 
 const AboutCard = () => {
@@ -11,7 +14,7 @@ const AboutCard = () => {
       <div className="row">
         <div className="col-md-6">
           <a href="https://carlnaza.github.io/parseSuperchargers/">
-                <img src="../../public/images/BudgetTesla.png" alt="budget tesla"/>
+                <img src="./images/BudgetTesla.png" alt="budget tesla"/>
             <div className="img-overlay">
               <h5 className="bottom-text">Budget Tesla</h5>
             </div>
@@ -19,26 +22,26 @@ const AboutCard = () => {
         </div>
         <div className="col-md-6">
           <a href="https://limitless-reef-57804.herokuapp.com/">
-            <img src="./assets/Images/logo_mrb.png" alt="my recipe box"/>
+            <img src="./images/logo_mrb.png" alt="my recipe box"/>
             <div className="img-overlay">
               <h5 className="bottom-text">My Recipe Box</h5>
             </div>
           </a>
         </div>
         <div className="col-md-6">
-          <img src="./assets/Images/head_shot.jpg" alt="rubber duck"/>
+          <img src="./images/head_shot.jpg" alt="rubber duck"/>
             <div className="img-overlay">
               <h5 className="bottom-text">Independent Projects</h5>
             </div>
            </div>
           <div className="col-md-6">
-              <img src="/public/images/balloon.jpg" alt="rubber duck"/>
+              <img src="./images/balloon.jpg" alt="rubber duck"/>
               <div className="img-overlay">
                 <h5 className="bottom-text">Balloon Collaboration</h5>
               </div>
            </div>
             <div className="col-md-6">
-              <img src="./assets/Images/octocat.jpg" alt="rubber duck"/>
+              <img src="./images/octocat.jpg" alt="rubber duck"/>
                 <div class="img-overlay">
                   <h5 class="bottom-text">Octocat Collaboration</h5>
                 </div>
